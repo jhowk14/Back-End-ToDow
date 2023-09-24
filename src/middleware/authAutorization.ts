572f2b77bad5b0c2b9dfbc727e0f26826a6814c0
jhowk14 <jonathanperozi16@gmail.com>
2023-09-24 12:10:08 -0300

    add api key in header

--- src/middleware/authAutorization.ts
+++ src/middleware/authAutorization.ts
@@ -6,23 +6,29 @@ const jwtSecret = process.env.JWT_SECRET;
 
 export const authAuthorization = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('Authorization');
+    const apiKey = req.headers['apikey']; // Use letras minúsculas para a chave 'apikey'
 
-    if (!token) {
-        throw new ApiError("Authorization token is missing", 401)
+    if (!token && !apiKey) {
+        throw new ApiError("Authorization token or API key is missing", 401);
     }
 
     if (typeof jwtSecret === 'undefined') {
-        throw new ApiError("JWT_SECRET is not defined", 500)
-
+        throw new ApiError("JWT_SECRET is not defined", 500);
     }
+
     try {
-        const decodedToken = verify(token, jwtSecret);
-        // Adicione o usuário autenticado ao objeto de solicitação para que as rotas subsequentes possam acessá-lo
-        req.userId = decodedToken;
+        if (token) {
+            const decodedToken = verify(token, jwtSecret);
+            // Adicione o usuário autenticado ao objeto de solicitação para que as rotas subsequentes possam acessá-lo
+            req.userId = decodedToken;
+        } else if (apiKey === process.env.API_KEY) {
+            req.userId = 'dev'
+        } else {
+            throw new ApiError("Unauthorized", 402);
+        }
 
         next(); // Avança para a próxima função middleware ou rota
     } catch (error) {
-        throw new ApiError("Unauthorized", 402)
-
+        throw new ApiError("Unauthorized", 402);
     }
 };
